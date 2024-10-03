1655fd9773c1abc4a16587300e08e4508b07980a
faizzaidi23 <786faizzaidi@gmail.com>
2024-10-03 06:53:37 +0530

    added new funtion of sum

--- file.js
+++ file.js
@@ -2,3 +2,9 @@ a=5;
 b=6;
 c=a+b
 console.log("the sum of the given two number is",c);
+
+let a=25;
+let b=36;
+let c=a+b;
+console.log("Ths sum of a and b is:",c);
+console.log("this change in the local-repo will be shown in the github repo having name local-repo")
